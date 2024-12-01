c7c63a1ae0a9afcc16ddb3e5b2c16302b2738eb1
Hustini <ejdersalih02@gmail.com>
2024-12-01 17:02:33 +0100

    added testComponent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestComponent></TestComponent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestComponent from './components/TestComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestComponent
   }
 }
 </script>
