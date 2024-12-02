1e749e503fe2ab2b2c5c9eb196b4a5118e2a19c3
Hustini <ejdersalih02@gmail.com>
2024-12-02 15:55:29 +0100

    added the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,30 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <header class="header">
+    <h1>OLIVER BENJAMIN RUTZ</h1>
+  </header>
 </template>
 
-<style scoped>
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
 
+<style>
+.header {
+  background-color: black;
+  color: white;
+  text-align: left;
+  padding: 1rem;
+  font-family: Arial, sans-serif;
+  font-size: 1.5rem;
+  position: relative;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+h1 {
+  margin: 0;
+  padding-left: 1rem;
+}
 </style>
