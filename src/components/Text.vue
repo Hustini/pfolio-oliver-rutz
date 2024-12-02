fc15fb4df211f18edf603ce7be25a1d0a58b3583
Hustini <ejdersalih02@gmail.com>
2024-12-02 15:54:50 +0100

    added this component

---
+++ src/components/Text.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text">
+    <h1>INTERACTIVE MEDIA DESIGN</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextDisplay'
+}
+</script>
+
+<style>
+.text {
+  background-color: black;
+  color: white;
+  text-align: left;
+  padding: 1rem;
+  font-family: Arial, sans-serif;
+  font-size: 1.5rem;
+  position: absolute;
+  width: 100%;
+  box-sizing: border-box;
+  bottom: 0;
+  left: 0;
+}
+
+h1 {
+  margin: 0;
+  padding-left: 1rem; /* Adds padding from the left */
+}
+</style>
