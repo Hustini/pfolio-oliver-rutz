d070b6fd825188bc779e651f1d3e593ba93e3f96
Hustini <ejdersalih02@gmail.com>
2024-12-04 09:50:51 +0100

    changed styling and class names

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,15 +1,10 @@
 <template>
   <section>
-    <h1 class="title">Titel</h1>
-
-    <div class="faq">
-      <div class="question">
-        <h3>What is Javascript</h3>
-        <svg width="15" height="10" viewBox="0 0 42 25">
-          <path d="M3 3L21 21L39 3" stroke="white" stroke-width="7" stroke-linecap="round"/>
-        </svg>
+    <div class="wrapper">
+      <div class="title">
+        <h3>About</h3>
       </div>
-      <div class="answer">
+      <div class="content">
         <p>
           JavaScript, often abbreviated as JS, is a programming language that
           conforms to the ECMAScript specification. JavaScript is high-level,
@@ -24,11 +19,11 @@
 export default {
   name: "AccordionPanel",
   mounted() {
-    const faqs = document.querySelectorAll(".faq");
+    const items = document.querySelectorAll(".wrapper");
 
-    faqs.forEach(faq => {
-      faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
+    items.forEach(item => {
+      item.addEventListener("click", () => {
+        item.classList.toggle("active");
       });
     });
   },
@@ -38,19 +33,33 @@ export default {
 <style>
 section {
   min-height: 100vh;
-  width: 80%;
-  margin: 0 auto;
+  width: 100%;
+  margin-top: -10px;
   display: flex;
   flex-direction: column;
-  align-items: center;
 }
 
 .title {
-  font-size: 3rem;
-  margin: 2rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+  color: white;
+  text-align: left;
+  position: absolute;
+  width: 100%;
+  box-sizing: border-box;
 }
 
-.faq {
+.title h3{
+  margin: 0;
+  padding-left: 1rem;
+  font-family: Helvetica, sans-serif;
+  font-weight: bold;
+  font-size: 72px;
+}
+
+.wrapper {
   max-width: 700px;
   margin-top: 2rem;
   padding-bottom: 1rem;
@@ -58,29 +67,19 @@ section {
   cursor: pointer;
 }
 
-.question {
-display: flex;
-justify-content: space-between;
-align-items: center;
-}
-
-.question h3 {
-  font-size: 1.8rem;
-}
-
-.answer {
+.content {
   max-height: 0;
   overflow: hidden;
   transition: max-height 0.3s ease;
 }
 
-.answer p {
-  padding-top: 1rem;
+.content p {
+  padding-top: 5rem;
   line-height: 1.6;
   font-size: 1.4rem;
 }
 
-.faq.active .answer {
+.wrapper.active .content {
   max-height: 300px;
 }
 </style>
