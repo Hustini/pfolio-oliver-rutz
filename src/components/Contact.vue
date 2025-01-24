41a0a1e70aa43d69b29bce43f422530aa05c1d8b
Hustini <ejdersalih02@gmail.com>
2025-01-24 16:22:39 +0100

    added the content for the contactCard component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,23 @@
 <template>
-  <p>HelloWorld</p>
+  <div class="flex-container">
+    <div class="socials-contact">
+      <h1>INSTA</h1>
+      <h1>MAIL</h1>
+      <h1>LINKEDIN</h1>
+    </div>
+    <div class="work-contact">
+      <div class="navigation">
+        <h1>HOME</h1>
+        <h1>ABOUT</h1>
+        <h1>WORK</h1>
+      </div>
+      <div class="navigation">
+        <h1>AGB's</h1>
+        <h1>Datenschutz</h1>
+        <h1>Impressum</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +27,23 @@ export default {
 </script>
 
 <style>
+.flex-container {
+  display: flex;
+  color: black;
+  justify-content: space-between;
+  margin: 50px 75px;
+}
 
+.socials-contact h1 {
+  font-size: 50px;
+}
+
+.navigation {
+  text-align: right;
+  margin-bottom: 45px;
+}
+
+.navigation h1 {
+  font-size: 18px;
+}
 </style>
