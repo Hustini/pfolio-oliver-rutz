14706cda0839d92c9b11dd3ca5dfeefcd98a5109
Hustini <ejdersalih02@gmail.com>
2025-05-06 17:14:52 +0200

    added new components

---
+++ src/components/Blob.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <p>Blob</p>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <p>Navbar</p>
+</template>
+
+<style scoped>
+</style>
