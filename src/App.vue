b038e6bf52cfb55ba42985ea792314c20f738d39
Hustini <ejdersalih02@gmail.com>
2025-05-06 18:00:32 +0200

    deleted the main.css and added the blob

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <script setup>
+import NavBar from "@/components/NavBar.vue";
+import Blob from "@/components/Blob.vue";
 </script>
 
 <template>
+  <NavBar class="navbar"></NavBar>
+  <div class="container">
+    <Blob></Blob>
+  </div>
 </template>
 
 <style scoped>
+.navbar {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 </style>
--- src/components/Blob.vue
+++ src/components/Blob.vue
@@ -2,8 +2,19 @@
 </script>
 
 <template>
-  <p>Blob</p>
+  <div class="blob"></div>
 </template>
 
 <style scoped>
+.blob {
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  background: radial-gradient(circle at 75% 25%,
+  rgba(0, 255, 64, 1) 0%,
+  rgba(0, 255, 64, 1) 10%,
+  rgb(119, 23, 244) 70%);
+  filter: blur(30px);
+  box-shadow: 0 0 40px rgba(0, 255, 64, 0.4), 0 0 60px rgba(119, 23, 244, 0.4);
+}
 </style>
