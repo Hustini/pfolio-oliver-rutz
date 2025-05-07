c802d5c2ca86a41f102e21a03b28f9b1d2591896
Hustini <ejdersalih02@gmail.com>
2025-05-07 15:59:47 +0200

    added the title

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,47 @@ import Blob from "@/components/Blob.vue";
 </script>
 
 <template>
-  <NavBar class="navbar"></NavBar>
-  <div class="container">
-    <Blob></Blob>
+  <NavBar class="navbar" />
+  <div class="hero">
+    <Blob class="blob" />
+    <h1 class="title">Interactive Media Designer Oliver Rutz</h1>
   </div>
 </template>
 
 <style scoped>
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+.hero {
+  position: relative;
   height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.blob {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+}
+
+.title {
+  position: relative;
+  z-index: 2;
+  font-size: 95px;
+  font-weight: 600;
+  text-align: left;
+  color: #000;
+  margin-top: auto;
+  padding: 0 40px 40px;
 }
 </style>
