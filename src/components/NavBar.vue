2ef159766cbe57cb708de0688859e39ed7403fcb
Hustini <ejdersalih02@gmail.com>
2025-05-07 15:23:22 +0200

    navbar is always closed and arrow rotates with animation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,34 @@
 <template>
   <nav class="navbar">
     <div class="navbar-container">
-      <a href="https://flowbite.com/" class="navbar-logo">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="logo" alt="Flowbite Logo" />
-        <span class="brand-name">Flowbite</span>
+      <a href="../app" class="navbar-logo">
+        <span class="brand-name">Home</span>
       </a>
       <button class="menu-toggle" aria-controls="navbar-menu" aria-expanded="false" @click="toggleMenu">
-        <span class="sr-only">Open main menu</span>
-        <svg class="menu-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        <svg  :class="['arrow-icon', { rotated: isMenuOpen }]" viewBox="0 -6.5 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg"
+             xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+          <g id="SVGRepo_iconCarrier"><title>right-arrow</title>
+            <desc>Created with Sketch.</desc>
+            <g id="icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+              <g id="ui-gambling-website-lined-icnos-casinoshunter" transform="translate(-212.000000, -159.000000)"
+                 fill="#252528" fill-rule="nonzero">
+                <g id="square-filled" transform="translate(50.000000, 120.000000)">
+                  <path
+                      d="M187.108012,39.2902857 L197.649804,49.7417043 L197.708994,49.7959169 C197.889141,49.9745543 197.986143,50.2044182 198,50.4382227 L198,50.5617773 C197.986143,50.7955818 197.889141,51.0254457 197.708994,51.2040831 L197.6571,51.2479803 L187.108012,61.7097143 C186.717694,62.0967619 186.084865,62.0967619 185.694547,61.7097143 C185.30423,61.3226668 185.30423,60.6951387 185.694547,60.3080911 L194.702666,51.3738496 L162.99947,51.3746291 C162.447478,51.3746291 162,50.9308997 162,50.3835318 C162,49.8361639 162.447478,49.3924345 162.99947,49.3924345 L194.46779,49.3916551 L185.694547,40.6919089 C185.30423,40.3048613 185.30423,39.6773332 185.694547,39.2902857 C186.084865,38.9032381 186.717694,38.9032381 187.108012,39.2902857 Z M197.115357,50.382693 L186.401279,61.0089027 L197.002151,50.5002046 L197.002252,50.4963719 L196.943142,50.442585 L196.882737,50.382693 L197.115357,50.382693 Z"
+                      id="right-arrow"></path>
+                </g>
+              </g>
+            </g>
+          </g>
         </svg>
       </button>
       <div :class="['navbar-menu', { active: isMenuOpen }]">
         <ul class="menu-items">
-          <li><a href="#" class="menu-item active">Home</a></li>
+          <li><a href="#" class="menu-item active">Menu</a></li>
+          <li><a href="#" class="menu-item">Projects</a></li>
           <li><a href="#" class="menu-item">About</a></li>
-          <li><a href="#" class="menu-item">Services</a></li>
-          <li><a href="#" class="menu-item">Pricing</a></li>
           <li><a href="#" class="menu-item">Contact</a></li>
         </ul>
       </div>
@@ -25,7 +37,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import {ref} from 'vue';
 
 const isMenuOpen = ref(false);
 
@@ -38,7 +50,7 @@ const toggleMenu = () => {
 /* General Navbar Styles */
 .navbar {
   background-color: #ffffff;
-  border-bottom: 1px solid #e0e0e0;
+  border-bottom: 1px solid #000000;
 }
 
 .navbar-container {
@@ -56,15 +68,22 @@ const toggleMenu = () => {
   text-decoration: none;
 }
 
-.logo {
-  height: 32px;
-}
-
 .brand-name {
   font-size: 24px;
   font-weight: 600;
   margin-left: 8px;
   color: #333;
+}
+
+.arrow-icon {
+  width: 20px;
+  height: 20px;
+  transform: rotate(270deg);
+  transition: transform 0.3s ease-in-out;
+}
+
+.arrow-icon.rotated {
+  transform: rotate(90deg);
 }
 
 /* Menu Toggle Button */
@@ -76,29 +95,24 @@ const toggleMenu = () => {
   cursor: pointer;
 }
 
-.menu-icon {
-  width: 20px;
-  height: 20px;
-  stroke: #333;
-}
-
 /* Navbar Menu */
 .navbar-menu {
-  display: none; /* Hide the menu by default */
+  display: none;
   position: absolute;
   top: 60px;
   left: 0;
   right: 0;
   width: 100%;
   background-color: #f8f8f8;
-  transition: all 0.3s ease-in-out; /* Smooth transition */
-  max-height: 0; /* Start with no height */
-  overflow: hidden; /* Hide overflow */
+  transition: all 0.3s ease-in-out;
+  max-height: 0;
+  overflow: hidden;
 }
 
 .navbar-menu.active {
-  display: block; /* Show the menu when active */
-  max-height: 500px; /* Set a max-height when menu is open (adjust as necessary) */
+  display: block;
+  max-height: 500px;
+
 }
 
 .menu-items {
