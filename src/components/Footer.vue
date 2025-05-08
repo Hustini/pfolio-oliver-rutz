efce8c5f9fc40888095989d185cc9509db7c746d
Hustini <ejdersalih02@gmail.com>
2025-05-08 13:03:17 +0200

    added more contact info and styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,26 @@
 
 <template>
   <footer>
-    <div>Let's Work Together!</div>
+    <div class="big-text">Let's Work Together!</div>
+    <div class="contact-container">
+      <div class="contact-item">
+        <div>Mail</div>
+        <div>Insta</div>
+      </div>
+      <div class="contact-item">
+        <div>olivverutz26@gmail.com</div>
+        <a href="https://www.instagram.com/oli_s_acc/">oli_s_acc</a>
+      </div>
+    </div>
   </footer>
 </template>
 
 <style scoped>
 footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   position: relative;
   margin: 0;
   width: 100%;
@@ -16,5 +30,23 @@ footer {
   background-color: #11FF004D;
   color: white;
   font-size: 14px;
+}
+
+.big-text {
+  font-size: 140px;
+  color : #000000;
+}
+
+.contact-container {
+  display: flex;
+  gap: 2rem;
+  align-items: flex-start;
+}
+
+.contact-item {
+  display: flex;
+  flex-direction: column;
+  font-size: 20px;
+  color: #000000;
 }
 </style>
