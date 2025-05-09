d387b1f7cf72cd3d16e82e2c51aca40fe1fe7405
Hustini <ejdersalih02@gmail.com>
2025-05-09 15:44:25 +0200

    used router to create a multi page app

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,16 @@
 <script setup>
-import NavBar from "@/components/NavBar.vue";
-import Blob from "@/components/Blob.vue";
-import Footer from "@/components/Footer.vue";
-import InfoSection from "@/components/InfoSection.vue";
-import ProjectParent from "@/components/ProjectParent.vue";
 </script>
 
 <template>
-  <div class="hero">
-    <div class="layout-container hero-content">
-      <Blob class="blob" width="400px" height="400px" />
-      <h1 class="title">Interactive Media Designer Oliver Rutz</h1>
-    </div>
-    <InfoSection />
-    <ProjectParent />
-    <Footer class="footer" />
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link>
+    |
+    <router-link to="/about">About</router-link>
+    |
+    <router-link to="/projects">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
 <style scoped>
-.hero {
-  position: relative;
-  width: 100%;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  background-color: white;
-}
-
-.hero-content {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  height: 100vh;
-  justify-content: center;
-}
-
-.blob {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 0;
-  pointer-events: none;
-}
-
-.title {
-  position: relative;
-  z-index: 1;
-  font-size: 6rem;
-  font-weight: 400;
-  text-align: left;
-  color: #000;
-  border: 2px solid black;
-  margin-top: auto;
-  padding: 1rem 0;
-}
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,60 @@
+<script setup>
+import InfoSection from "@/components/InfoSection.vue";
+import Blob from "@/components/Blob.vue";
+import Footer from "@/components/Footer.vue";
+import ProjectParent from "@/components/ProjectParent.vue";
+</script>
+
+<template>
+  <div class="hero">
+    <div class="layout-container hero-content">
+      <Blob class="blob" width="400px" height="400px" />
+      <h1 class="title">Interactive Media Designer Oliver Rutz</h1>
+    </div>
+    <InfoSection />
+    <ProjectParent />
+    <Footer class="footer" />
+  </div>
+</template>
+
+<style scoped>
+.hero {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  background-color: white;
+}
+
+.hero-content {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  height: 100vh;
+  justify-content: center;
+}
+
+.blob {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 0;
+  pointer-events: none;
+}
+
+.title {
+  position: relative;
+  z-index: 1;
+  font-size: 6rem;
+  font-weight: 400;
+  text-align: left;
+  color: #000;
+  border: 2px solid black;
+  margin-top: auto;
+  padding: 1rem 0;
+}
+</style>
