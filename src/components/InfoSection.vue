7c94050557f0795f43af3760472c993a6bfa367c
Hustini <ejdersalih02@gmail.com>
2025-05-09 14:08:43 +0200

    new info section

---
+++ src/components/InfoSection.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Button from "@/components/Button.vue";
+</script>
+
+<template>
+  <div class="info-container">
+    <div class="layout-container">
+      <Button buttonText="LEARN MORE ABOUT ME" isArrow="True"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.info-container {
+  background-color: #11FF004D;
+}
+</style>
