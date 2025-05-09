4bb7728b8357bfb3d36a1b8c7b69c7affd0399b5
Hustini <ejdersalih02@gmail.com>
2025-05-09 15:54:39 +0200

    new arrow and renamed class

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,13 @@ import ProjectParent from "@/components/ProjectParent.vue";
 </script>
 
 <template>
-  <div class="hero">
+  <div class="home">
     <div class="layout-container hero-content">
       <Blob class="blob" width="400px" height="400px" />
       <h1 class="title">Interactive Media Designer Oliver Rutz</h1>
+      <svg class="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <path d="M26.29 20.29 18 28.59V0h-2v28.59l-8.29-8.3-1.42 1.42 10 10a1 1 0 0 0 1.41 0l10-10z" data-name="2-Arrow Down"/>
+      </svg>
     </div>
     <InfoSection />
     <ProjectParent />
@@ -18,7 +21,7 @@ import ProjectParent from "@/components/ProjectParent.vue";
 </template>
 
 <style scoped>
-.hero {
+.home {
   position: relative;
   width: 100%;
   overflow: hidden;
@@ -56,5 +59,13 @@ import ProjectParent from "@/components/ProjectParent.vue";
   border: 2px solid black;
   margin-top: auto;
   padding: 1rem 0;
+}
+
+.arrow {
+  position: absolute;
+  bottom: 13.5%;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 20px;
 }
 </style>
