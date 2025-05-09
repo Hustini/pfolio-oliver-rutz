f6e98d15ccf08c6e4982ca158f114c4240c081b5
Hustini <ejdersalih02@gmail.com>
2025-05-09 15:07:07 +0200

    finished ProjectCard.vue

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,9 +1,47 @@
 <script setup>
+const props = defineProps({
+  imgPath: {
+    type: [Number, String],
+    default: "/img/placeholder.png"
+  },
+  title: {
+    type: [Number, String],
+    default: "default title"
+  },
+  caption: {
+    type: [Number, String],
+    default: "default caption"
+  }
+})
 </script>
 
 <template>
-  <h1>TestTestTestTest</h1>
+  <div>
+    <img :src="imgPath" alt="placeholder">
+    <div class="text">
+      <div class="title">{{title}}</div>
+      <div class="caption">{{caption}}</div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+img {
+  width: 425px;
+  border: solid 1px black;
+}
+
+.text {
+  margin-top: 0.5rem;
+}
+
+.title {
+  font-weight: 400;
+  font-size: 20px;
+}
+
+.caption {
+  font-weight: 400;
+  font-size: 12px;
+}
 </style>
