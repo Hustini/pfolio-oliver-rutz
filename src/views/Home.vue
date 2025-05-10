fde907a44f7c4e8236032033caeadb39ffa9c609
Hustini <ejdersalih02@gmail.com>
2025-05-10 18:13:32 +0200

    test percent padding on deployment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -90,7 +90,7 @@ import ProjectParent from "@/components/ProjectParent.vue";
     margin: 0;
     width: 100vh;
     height: auto;
-    padding-top: 250px;
+    padding-top: 60%;
     padding-left: 30px;
     z-index: 1;
   }
