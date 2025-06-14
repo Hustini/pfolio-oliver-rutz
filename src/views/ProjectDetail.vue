868c921a0fb517237faab6d11275224ee1a520a7
Hustini <ejdersalih02@gmail.com>
2025-06-14 15:12:56 +0200

    page content done

--- src/views/ProjectDetail.vue
+++ src/views/ProjectDetail.vue
@@ -3,6 +3,9 @@ import Footer from '@/components/Footer.vue';
 import NavBar from "@/components/NavBar.vue";
 import { ref } from 'vue'
 import { useRoute } from 'vue-router';
+import Breadcrumb from '@/components/Breadcrumb.vue';
+import ProjectCard from '@/components/ProjectCard.vue';
+import Button from '@/components/Button.vue';
 
 const route = useRoute();
 const imgPath = route.params.imgPath;
@@ -20,16 +23,29 @@ function scrollTo() {
 </script>
 
 <template>
-  <div class="project-detail-container">
+  <div class="page-container">
     <NavBar isArrow="True" @scroll-contact="() => scrollTo(scrollToContact)" />
     <div class="layout-container">
-      <div class="test">
-        <div>{{ imgPath }}</div>
-        <div>{{ title }}</div>
-        <div>{{ time }}</div>
-        <div class="blue" v-for="tag in tagsSplit">{{ tag }}</div>
-        <div>{{ text }}</div>
-        <div>{{ link }}</div>
+      <div class="padding-bottom">
+        <Breadcrumb />
+      </div>
+      <div class="project">
+        <div class="project-info">
+          <div class="wrapper"> <!--Besser Platzieren -->
+            <ProjectCard :imgPath="imgPath" title="" caption=""/>
+            <div class="title">{{ title }}</div>
+          </div>
+          <div class="wrapper">
+            Tags: <Button class="tag" v-for="tag in tagsSplit" :buttonText="tag"/>
+          </div>
+          <div class="wrapper">
+            Zeitraum: {{ time }}
+          </div>
+        </div>
+        <div class="project-text">
+          <div>{{ text }}</div>
+          <a :href="link" target="_blank">{{ link }}</a>
+        </div>
       </div>
     </div>
     <Footer ref="scrollToContact" />
@@ -37,16 +53,36 @@ function scrollTo() {
 </template>
 
 <style scoped>
-.project-detail-container {
+.page-container {
   border-bottom: black solid 1px;
 }
 
-.blue {
-  color: blue;
+.padding-bottom {
+  padding-bottom: 2rem;
 }
 
-.test {
+.project {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  gap: 2rem;
+}
+
+.project-info {
+  max-width: 50%;
+  min-width: 50%;
+}
+
+.title {
+  font-size: 2.5rem;
+}
+
+.wrapper {
+  font-size: 40px;
+  padding-bottom: 2rem;
+}
+
+.tag {
+  background: #7717F44D;
+  margin-right: 1rem;
 }
 </style>
