7f361afcca08e09c38f784b5cc02c31ce5f0e43e
Hustini <ejdersalih02@gmail.com>
2025-06-19 15:17:54 +0200

    added links to the projectsDetails page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -21,14 +21,20 @@ function scrollTo() {
       <div class="title">Projects</div>
       <div class="featured-title">Featured Projects </div>
       <div class="featured-projects">
-        <div class="wrapper">
+        <router-link
+            :to="{ name: 'ProjectDetail', params: { imgPath: '/img/placeholder.png', title: 'Feature 1', tags: 'HTML,CSS,JS', time: '2025', text: 'In meinem Projekt «Zürich  Reimagined» stelle ich Graffiti als künstlerische Ausdrucksform im Raum  Zürich dar. Oft als Vandalismus abgetan, sehe ich Graffiti als ein  Sprachrohr für kreative Freiheit und städtische Identität. Mein Ziel war es, eine Kultur des Miteinanders und der Kooperation zu fördern, indem  ich Möglichkeit legale Graffiti-Flächen zu unterstützen biete und einen  Dialog zwischen Künstlern, der Community und Behörden anrege. Dafür habe ich eine digitale Awareness-Kampagne in Form einer Website konzipiert,  die über die Geschichte, Bedeutung und rechtlichen Rahmenbedingungen von Graffiti informiert. Das Design kombiniert Swiss-Layout-Prinzipien mit  künstlerischen Elementen der Streetart. ', link: 'https://www.figma.com/proto/cwxJ3KXRWbxD2EYGdkAZ8Y/Z%C3%BCrich-Reimagined-Schule?page-id=582%3A537&node-id=582-803&viewport=848%2C270%2C0.08&t=Gw1DZNUiNjJdjqmJ-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=582%3A803&show-proto-sidebar=1'}}"
+            class="link"
+        >
+          <ProjectCard imgPath="/img/placeholder.png" title="" caption=""/>
+          <div class="featured-title">Feature 1</div>
+        </router-link>
+        <router-link
+            :to="{ name: 'ProjectDetail', params: { imgPath: '/img/placeholder.png', title: 'Feature 2', tags: 'HTML,CSS,JS', time: '2025', text: 'In meinem Projekt «Zürich  Reimagined» stelle ich Graffiti als künstlerische Ausdrucksform im Raum  Zürich dar. Oft als Vandalismus abgetan, sehe ich Graffiti als ein  Sprachrohr für kreative Freiheit und städtische Identität. Mein Ziel war es, eine Kultur des Miteinanders und der Kooperation zu fördern, indem  ich Möglichkeit legale Graffiti-Flächen zu unterstützen biete und einen  Dialog zwischen Künstlern, der Community und Behörden anrege. Dafür habe ich eine digitale Awareness-Kampagne in Form einer Website konzipiert,  die über die Geschichte, Bedeutung und rechtlichen Rahmenbedingungen von Graffiti informiert. Das Design kombiniert Swiss-Layout-Prinzipien mit  künstlerischen Elementen der Streetart. ', link: 'https://www.figma.com/proto/cwxJ3KXRWbxD2EYGdkAZ8Y/Z%C3%BCrich-Reimagined-Schule?page-id=582%3A537&node-id=582-803&viewport=848%2C270%2C0.08&t=Gw1DZNUiNjJdjqmJ-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=582%3A803&show-proto-sidebar=1'}}"
+            class="link"
+        >
           <ProjectCard class="featured-title" imgPath="/img/placeholder.png" title="" caption=""/>
-          <div class="featured-title">gkjhdgijb fasdkfsfsd</div>
-        </div>
-        <div class="wrapper">
-          <ProjectCard class="featured-title" imgPath="/img/placeholder.png" title="" caption=""/>
-          <div class="featured-title">gkjhdgijb fasdkfsfsd</div>
-        </div>
+          <div class="featured-title">Feature 2</div>
+        </router-link>
       </div>
     </div>
     <Filter />
@@ -54,6 +60,11 @@ function scrollTo() {
 
 .project-container {
   padding-top: 2rem;
+}
+
+.link {
+  text-decoration: none;
+  color: black;
 }
 
 @media (max-width: 640px) {
